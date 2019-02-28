0380028f2e4f87555cfb8ad09692d63bf7f80292
Daniel Lew <dylew@ucsd.edu>
2019-02-28 10:23:55 -0800

    lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,16 @@
 function initMap() {
-	// add your code here
+	
+	L.mapquest.key = 'z55A4lFjgZ4oti0fuRf5SjD4dtXgn2sG';
+
+	//'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.878802, -117.235910],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	})
+
+	//Add a marker to the map
+	L.marker([32.878802, -117.235910]).addTo(map);
+
 }
